4f73b5b484f931cde189e7604d42053449deab9c
Arman Frasier <armanfrasier@gmail.com>
2017-08-14 12:49:27 -0400

    Update tests, add pagination

--- talentmap_api/settings.py
+++ talentmap_api/settings.py
@@ -104,6 +104,7 @@ TEMPLATES = [
 
 # Rest framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'talentmap_api.common.pagination.ControllablePageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': (
         'talentmap_api.common.filters.DisabledHTMLFilterBackend',
         'rest_framework.filters.OrderingFilter'
