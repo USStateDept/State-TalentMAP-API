755673666d9be426c2c854f6a5fd3c1d78dea2c2
Arman Frasier <armanfrasier@gmail.com>
2017-08-28 14:35:04 -0400

    Add paramable field limiting and exclusions, tests

--- talentmap_api/common/mixins.py
+++ talentmap_api/common/mixins.py
@@ -5,3 +5,26 @@ class ActionDependentSerializerMixin(object):
 
     def get_serializer_class(self):
             return self.serializers.get(self.action, self.serializers['default'])
+
+
+class FieldLimitableSerializerMixin(object):
+    '''
+    Supports limiting the return fields via the "fields" and "fields_exclude"
+    query parameters from a request by passing the data into the serializer via
+    the serializer context. Only works with PrefetchedSerializer descendants
+    '''
+
+    def get_serializer_context(self):
+        context = super(FieldLimitableSerializerMixin, self).get_serializer_context()
+
+        include_param_name = "include"
+        exclude_param_name = "exclude"
+
+        # Check query params for "include", which are included fields
+        if include_param_name in self.request.query_params:
+            context = {**context, "override_fields": self.request.query_params.get(include_param_name).split(',')}
+        # Check query params for "exclude", which are excluded fields
+        if exclude_param_name in self.request.query_params:
+            context = {**context, "override_exclude": self.request.query_params.get(exclude_param_name).split(',')}
+
+        return context
