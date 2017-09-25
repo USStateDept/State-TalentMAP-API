af861436ced069c3d068bbf65e457fc000ae721a
Arman Frasier <armanfrasier@gmail.com>
2017-09-25 13:48:29 -0400

    Add bid list routes and actions

--- talentmap_api/bidding/models.py
+++ talentmap_api/bidding/models.py
@@ -11,8 +11,12 @@ class BidCycle(models.Model):
     name = models.TextField(null=False, help_text="The name of the bid cycle")
     cycle_start_date = models.DateField(null=False, help_text="The start date for the bid cycle")
     cycle_end_date = models.DateField(null=False, help_text="The end date for the bid cycle")
+    active = models.BooleanField(default=False, help_text="Whether this bidcycle is active or not")
 
     positions = models.ManyToManyField('position.Position', related_name="bid_cycles")
+
+    def __str__(self):
+        return f"{self.name} ({self.cycle_start_date} - {self.cycle_end_date})"
 
     class Meta:
         managed = True
@@ -32,6 +36,12 @@ class Bid(models.Model):
 
     status = models.TextField(default=Status.draft, choices=Status.choices)
 
-    bidcycle = models.ForeignKey('bidding.BidCycle', on_delete=models.CASCADE, help_text="The bidcycle for this bid")
-    user = models.ForeignKey('user_profile.UserProfile', on_delete=models.CASCADE, help_text="The user owning this bid")
-    position = models.ForeignKey('position.Position', on_delete=models.CASCADE, help_text="The position this bid is for")
+    bidcycle = models.ForeignKey('bidding.BidCycle', on_delete=models.CASCADE, related_name="bids", help_text="The bidcycle for this bid")
+    user = models.ForeignKey('user_profile.UserProfile', on_delete=models.CASCADE, related_name="bidlist", help_text="The user owning this bid")
+    position = models.ForeignKey('position.Position', on_delete=models.CASCADE, related_name="bids", help_text="The position this bid is for")
+
+    submission_date = models.DateField(null=True)
+
+    class Meta:
+        managed = True
+        ordering = ["bidcycle__cycle_start_date", "submission_date"]
---
+++ talentmap_api/bidding/tests/test_bidlist.py
@@ -0,0 +1,90 @@
+import pytest
+
+from model_mommy import mommy
+from rest_framework import status
+
+from talentmap_api.bidding.models import BidCycle
+
+
+# Might move this fixture to a session fixture if we end up needing languages elsewhere
+@pytest.fixture
+def test_bidlist_fixture():
+    bidcycle = mommy.make(BidCycle, id=1, name="Bidcycle 1", active=True)
+    for i in range(5):
+        bidcycle.positions.add(mommy.make('position.Position'))
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.usefixtures("test_bidlist_fixture")
+def test_bidist_position_actions(authorized_client, authorized_user):
+    in_cycle_position = BidCycle.objects.first().positions.first()
+    out_of_cycle_position = mommy.make('position.Position')
+
+    # Check if our in cycle position is in the current user's bidlist
+    response = authorized_client.get(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    # Put the position into the bidlist
+    response = authorized_client.put(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Validate it is now in the list
+    response = authorized_client.get(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Remove the position from the list
+    response = authorized_client.delete(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Validate it was removed
+    response = authorized_client.get(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    # Attempt to add an out of cycle position
+    response = authorized_client.put(f'/api/v1/bidlist/position/{out_of_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.usefixtures("test_bidlist_fixture")
+def test_bidist_bid_actions(authorized_client, authorized_user):
+    in_cycle_position = BidCycle.objects.first().positions.first()
+
+    # Put the position into the bidlist
+    response = authorized_client.put(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Get our bidlist, and validate that the position is in the list as a draft
+    response = authorized_client.get(f'/api/v1/bidlist/')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["results"][0]["status"] == "draft"
+    assert response.data["results"][0]["position"] == str(in_cycle_position)
+    assert response.data["results"][0]["submission_date"] is None
+
+    bid_id = response.data["results"][0]["id"]
+
+    # Submit our bid
+    response = authorized_client.put(f'/api/v1/bidlist/bid/{bid_id}/submit/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Get our bidlist, and validate that the position is now "submitted"
+    response = authorized_client.get(f'/api/v1/bidlist/')
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["results"][0]["status"] == "submitted"
+    assert response.data["results"][0]["position"] == str(in_cycle_position)
+    assert response.data["results"][0]["submission_date"] is not None
+
+    # Try to delete our now submitted bid
+    response = authorized_client.delete(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
