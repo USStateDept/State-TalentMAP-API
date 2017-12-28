cb0b23f936a57662273880a1cf8f193ac9797040
Arman Frasier <armanfrasier@gmail.com>
2017-12-28 14:06:34 -0500

    Basic bidcycle statistics

--- talentmap_api/bidding/urls/bidcycle.py
+++ talentmap_api/bidding/urls/bidcycle.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.BidCycleView.as_view({**get_retrieve, **patch_update}), name="bidding.BidCycle-details"),
     url(r'^(?P<pk>[0-9]+)/position/(?P<pos_id>[0-9]+)/$', views.BidCyclePositionActionView.as_view(), name='bidding.BidCycle-position-actions'),
     url(r'^(?P<pk>[0-9]+)/positions/$', views.BidCycleListPositionView.as_view(get_list), name="bidding.BidCycle-list-positions"),
-    url(r'^(?P<pk>[0-9]+)/position/batch/(?P<saved_search_id>[0-9]+)/$', views.BidCycleBatchPositionActionView.as_view(), name="bidding.BidCycle-position-batch")
+    url(r'^(?P<pk>[0-9]+)/position/batch/(?P<saved_search_id>[0-9]+)/$', views.BidCycleBatchPositionActionView.as_view(), name="bidding.BidCycle-position-batch"),
+
+    url(r'^statistics/$', views.BidCycleStatisticsView.as_view({**get_list})),
+    url(r'^(?P<pk>[0-9]+)/statistics/$', views.BidCycleStatisticsView.as_view({**get_retrieve}), name="bidding.BidCycle-details"),
 ]
