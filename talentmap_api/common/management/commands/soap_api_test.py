f0c3d916e9914ec7946fddeeb07e4f2f8d05a021
Arman Frasier <armanfrasier@gmail.com>
2018-01-10 07:31:45 -0500

    Update SOAP test to play nice with self signed certs

--- talentmap_api/common/management/commands/soap_api_test.py
+++ talentmap_api/common/management/commands/soap_api_test.py
@@ -6,6 +6,10 @@ import zeep
 
 import defusedxml.lxml as ET
 
+from requests import Session
+from zeep.transports import Transport
+
+from talentmap_api.common.common_helpers import xml_etree_to_dict
 
 class Command(BaseCommand):
     help = 'Tests the connection to the SOAP webservices'
@@ -19,11 +23,18 @@ class Command(BaseCommand):
         parser.add_argument('arguments', nargs='*', type=str, help="The arguments for the command")
 
     def handle(self, *args, **options):
+        # Initialize transport layer
+        session = Session()
+        # Uncomment the following line if we're verifying a self signed certificate
+        # session.verify = 'path_to_self_signed.cert'
+        session.verify = False
+        transport = Transport(session=session)
+
         # Get the WSDL location
         wsdl_location = os.environ.get('WSDL_LOCATION')
         self.logger.info(f'Initializing client with WSDL: {wsdl_location}')
 
-        client = zeep.Client(wsdl=wsdl_location)
+        client = zeep.Client(wsdl=wsdl_location, transport=transport)
 
         if not options['command']:
             self.logger.info('No command specified, dumping wsdl information')
@@ -32,9 +43,12 @@ class Command(BaseCommand):
 
         self.logger.info(f'Calling command {options["command"]} with parameters {options["arguments"]}')
         response = getattr(client.service, options['command'])(*options['arguments'])
+        dict_response = xml_etree_to_dict(response)
         self.logger.info(type(response))
         if not isinstance(response, str):
             response = ET.tostring(response, pretty_print=True)
 
         self.logger.info(f'SOAP call response:')
         self.logger.info(response.decode('unicode_escape'))
+
+        self.logger.info(f'Dictionary parsed response: {dict_response}')
