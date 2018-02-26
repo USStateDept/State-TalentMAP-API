d6c560dd5309c9bb1857e952450933b0c2aaeb1e
Arman Frasier <armanfrasier@gmail.com>
2018-02-26 09:17:01 -0500

    Add IE11 Middleware

---
+++ talentmap_api/common/middleware.py
@@ -0,0 +1,10 @@
+
+class IE11Middleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Cache-Control'] = "no-cache,no-store"
+        return response
