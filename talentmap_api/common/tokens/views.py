6448807dd2ac52c6c01aae620c26f5c59664a244
Arman Frasier <armanfrasier@gmail.com>
2018-03-05 15:05:02 -0500

    Endpoints for viewing Auth tokens

---
+++ talentmap_api/common/tokens/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.authtoken.models import Token
+
+from talentmap_api.common.tokens.serializers import TokenSerializer
+
+
+class TokenView(mixins.RetrieveModelMixin,
+                GenericViewSet):
+    """
+    retrieve:
+    Return the current user's authentication token
+    """
+
+    serializer_class = TokenSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return get_object_or_404(Token, user=self.request.user)
