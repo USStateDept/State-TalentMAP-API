144f07d414080522eec25d979a52c02dc7f72ec5
Arman Frasier <armanfrasier@gmail.com>
2018-03-07 19:23:39 -0500

    Store and allow to filter on available position
    - Add status_code and status to positions
    - Allow to filter on new fields
    - Add priority ordering to synchronization jobs
    - Add code to pull in status code, status, and effective date
    (available position update date) from webservices

--- talentmap_api/integrations/management/commands/schedule_synchronization_job.py
+++ talentmap_api/integrations/management/commands/schedule_synchronization_job.py
@@ -14,23 +14,24 @@ class Command(BaseCommand):
 
         # Jobs, as a (model, delta) keypair
         self.default_jobs = [
-            ('position.Skill', SynchronizationJob.TIME_WEEK),
-            ('position.Grade', SynchronizationJob.TIME_WEEK),
-            ('position.SkillCone', SynchronizationJob.TIME_WEEK),
-            ('position.CapsuleDescription', SynchronizationJob.TIME_DAY),
-            ('organization.TourOfDuty', SynchronizationJob.TIME_WEEK),
-            ('organization.Organization', SynchronizationJob.TIME_WEEK),
-            ('organization.Country', SynchronizationJob.TIME_WEEK),
-            ('organization.Location', SynchronizationJob.TIME_WEEK),
-            ('organization.Post', SynchronizationJob.TIME_WEEK),
-            ('language.Language', SynchronizationJob.TIME_WEEK),
-            ('position.Position', SynchronizationJob.TIME_DAY),
-            ('bidding.BidCycle', SynchronizationJob.TIME_DAY),
+            ('position.Skill', SynchronizationJob.TIME_WEEK, 0),
+            ('position.Grade', SynchronizationJob.TIME_WEEK, 0),
+            ('position.SkillCone', SynchronizationJob.TIME_WEEK, 0),
+            ('position.CapsuleDescription', SynchronizationJob.TIME_DAY, 10),
+            ('organization.TourOfDuty', SynchronizationJob.TIME_WEEK, 0),
+            ('organization.Organization', SynchronizationJob.TIME_WEEK, 0),
+            ('organization.Country', SynchronizationJob.TIME_WEEK, 0),
+            ('organization.Location', SynchronizationJob.TIME_WEEK, 0),
+            ('organization.Post', SynchronizationJob.TIME_WEEK, 0),
+            ('language.Language', SynchronizationJob.TIME_WEEK, 0),
+            ('position.Position', SynchronizationJob.TIME_DAY, 0),
+            ('bidding.BidCycle', SynchronizationJob.TIME_HOUR, 1),
         ]
 
     def add_arguments(self, parser):
         parser.add_argument('model', nargs='?', type=str, help="The model to schedule")
         parser.add_argument('delta', nargs='?', type=int, help="The delta synchronization in seconds")
+        parser.add_argument('priority', nargs='?', type=int, help="The run priority of the job (0 is first)")
         parser.add_argument('--list', dest='list', action='store_true', help='Lists all synchronization jobs')
         parser.add_argument('--remove', dest='remove', action='store_true', help='Removes the specified model')
         parser.add_argument('--reset', dest='reset', action='store_true', help='Resets the last synchronization date on the model')
@@ -66,6 +67,7 @@ class Command(BaseCommand):
             return
 
         job.delta_synchronization = options['delta']
+        job.priority = options['priority']
         job.save()
         job.refresh_from_db()
 
@@ -76,7 +78,7 @@ class Command(BaseCommand):
             print(job)
 
     def set_defaults(self):
-        for model, delta in self.default_jobs:
+        for model, delta, priority in self.default_jobs:
             if not SynchronizationJob.objects.filter(talentmap_model=model):
-                self.logger.info(f'Creating default synchronization job {model} ∆{delta}')
-                SynchronizationJob.objects.create(talentmap_model=model, delta_synchronization=delta)
+                self.logger.info(f'Creating default synchronization job {model} ∆{delta} P{priority}')
+                SynchronizationJob.objects.create(talentmap_model=model, delta_synchronization=delta, priority=priority)
