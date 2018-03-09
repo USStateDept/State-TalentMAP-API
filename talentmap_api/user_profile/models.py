af8e6c91fc94466385590d138877a160095e734a
578556 <natburgwyn@bah5cg5233xs7.resource.ds.bah.com>
2018-03-08 22:33:11 -0500

    Clearing whitespace

--- talentmap_api/user_profile/models.py
+++ talentmap_api/user_profile/models.py
@@ -55,7 +55,7 @@ class UserProfile(StaticRepresentationModel):
             display_name = self.user.username
         else:
             display_name = self.user.email
-        
+
         return display_name
 
     @property
