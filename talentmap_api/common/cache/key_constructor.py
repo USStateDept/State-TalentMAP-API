82142bac34474cc5e3099a926c9f12a70cb01af4
Arman Frasier <armanfrasier@gmail.com>
2018-03-14 13:29:31 -0400

    Bandit fix

--- talentmap_api/common/cache/key_constructor.py
+++ talentmap_api/common/cache/key_constructor.py
@@ -23,7 +23,7 @@ class TalentMAPKeyConstructor(DefaultKeyConstructor):
     path_bit = PathKeyBit()
     request_params = bits.QueryParamsKeyBit()
 
-    def prepare_key(self, key_dict):
+    def prepare_key(self, key_dict):  # nosec We're OK to use MD5 here since it isn't for cryptographic purposes
         key_dict = order_dict(key_dict)  # We order the dict to ensure something like ?q=german&code=1 == ?code=1&q=german
         key_dict = json.dumps(key_dict)
         key_hex = hashlib.md5(key_dict.encode('utf-8')).hexdigest()
