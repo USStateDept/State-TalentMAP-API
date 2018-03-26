1d24f83a2f1ee9259ee3dac1640b9a4e7e514374
Arman Frasier <armanfrasier@gmail.com>
2018-03-25 23:24:20 -0400

    Allow 'in' on integer filters

--- talentmap_api/common/filters.py
+++ talentmap_api/common/filters.py
@@ -11,7 +11,7 @@ from django.core.exceptions import FieldDoesNotExist
 DATETIME_LOOKUPS = ['exact', 'gte', 'gt', 'lte', 'lt', 'range', 'year',
                     'month', 'day', 'hour', 'minute', 'second']
 DATE_LOOKUPS = DATETIME_LOOKUPS[:-3]
-INTEGER_LOOKUPS = ['exact', 'gte', 'gt', 'lte', 'lt', 'range']
+INTEGER_LOOKUPS = ['exact', 'gte', 'gt', 'lte', 'lt', 'range', 'in']
 BASIC_TEXT_LOOKUPS = ['exact', 'iexact', 'startswith', 'istartswith',
                       'endswith', 'iendswith']
 ALL_TEXT_LOOKUPS = BASIC_TEXT_LOOKUPS + ['contains', 'icontains', 'in', 'isnull']
