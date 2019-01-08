b6c4e05e27d536cb77b303932ebfe057a2d3dba9
Rob Tirserio <rtirserio@gmail.com>
2019-01-08 12:09:19 -0500

    feature: TM-372 - add route for public profile access

--- talentmap_api/user_profile/tests/test_user_profile_endpoints.py
+++ talentmap_api/user_profile/tests/test_user_profile_endpoints.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 from rest_framework_expiring_authtoken.models import ExpiringToken
 
+from talentmap_api.user_profile.models import UserProfile
 
 @pytest.fixture()
 def test_user_profile_fixture():
@@ -23,6 +24,14 @@ def test_user_token_endpoint(authorized_client, authorized_user, test_user_profi
 
     assert resp.status_code == status.HTTP_200_OK
 
+@pytest.mark.django_db(transaction=True)
+def test_user_public_profile_endpoint(authorized_client, authorized_user, test_user_profile_fixture):
+    user_profile = UserProfile.objects.last()
+
+    resp = authorized_client.get(f'/api/v1/profile/{user_profile.id}/')
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.data["username"] == user_profile.user.username
 
 @pytest.mark.django_db(transaction=True)
 def test_user_profile_current_assignment(authorized_client, authorized_user, test_user_profile_fixture):
