02c9ff1935bd07032c1aef63ac2522093bcb3409
Rob Tirserio <rtirserio@gmail.com>
2019-03-12 12:45:33 -0400

    fix: allow positions with a handshake offer to accept other bids. TM-212

--- talentmap_api/bidding/tests/test_bidlist.py
+++ talentmap_api/bidding/tests/test_bidlist.py
@@ -94,7 +94,7 @@ def test_bidlist_position_actions(authorized_client, authorized_user):
     # Try to make a bid on a position with a handshake
     response = authorized_client.put(f'/api/v1/bidlist/position/{in_cycle_position.id}/')
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.status_code == status.HTTP_204_NO_CONTENT
 
     bid.status = Bid.Status.handshake_accepted
     bid.save()
