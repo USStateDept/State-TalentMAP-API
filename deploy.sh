a8c09d160495f4b6b2f979224bae7b6c9ab007e3
Rob Tirserio <rtirserio@gmail.com>
2019-04-01 10:42:08 -0400

    chore: deploy script for quick deployments (#48)
    * chore: deploy script for quick deployments. also removed gevent and updated gitignore
    
    * chore: fix a few things
    
    * fix: remove db url
    
    * chore: add completion msg

---
+++ deploy.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+# "Automation" of the API app deployment.
+# * Sets up virtual env
+# * Gets code from the `dev` branch
+# * sets up environment vars
+# * install dependencies
+# * run migrations
+# * restart server
+
+# Create and activate virtual env
+virtualenv --python=/usr/bin/python3 venv
+source venv/bin/activate
+
+curr_date=`date "+%Y-%m-%d_%H-%M-%S"`
+
+# backup current version if it exists
+[ -d State-TalentMAP-API-dev ] && mv State-TalentMAP-API-dev State-TalentMAP-API-dev-$curr_date
+
+# get code
+wget -O dev.zip https://github.com/MetaPhase-Consulting/State-TalentMAP-API/archive/dev.zip
+
+unzip -o dev.zip
+rm dev.zip
+cd State-TalentMAP-API-dev
+
+# copy env setup script
+cp EXAMPLE_setup_environment.sh setup_environment.sh
+# setup env
+source setup_environment.sh
+
+export DATABASE_URL='<DATABASE_URL>'
+export DJANGO_LOG_DIRECTORY='/home/ec2-user/log/'
+export DEPLOYMENT_LOCATION='/home/ec2-user/State-TalentMAP-API-dev/'
+
+# install dependencies
+pip install -r requirements.txt
+
+python manage.py migrate
+
+# Stop the server
+pkill -f runserver
+
+python manage.py runserver 0.0.0.0:8000 &
+
+echo "Deployment complete"
+
+exit
