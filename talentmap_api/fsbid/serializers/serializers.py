9b481a87c286d8e8d026007f4cad83c13b26714d
Rob Tirserio <rtirserio@gmail.com>
2019-04-16 16:17:06 -0400

    tidy up a bit

--- talentmap_api/fsbid/serializers/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class FSBidSerializer(serializers.Serializer):
-  id = serializers.IntegerField(read_only=True)
-  
-
--- talentmap_api/fsbid/services.py
+++ talentmap_api/fsbid/services.py
@@ -13,7 +13,7 @@ def user_bids(employee_id, position_id=None):
     Get bids for a user on a position or all if no position
   '''
   bids = requests.get(f"{API_ROOT}/bids/?employeeId={employee_id}").json()
-  return [bid for bid in bids if bid['cyclePosition']['cp_id'] == int(position_id)] if position_id else bids
+  return [fsbid_bid_to_talentmap_bid(bid) for bid in bids if bid['cyclePosition']['cp_id'] == int(position_id)] if position_id else map(fsbid_bid_to_talentmap_bid, bids)
 
 def bid_on_position(userId, employeeId, cyclePositionId):
   '''
@@ -23,3 +23,43 @@ def bid_on_position(userId, employeeId, cyclePositionId):
 
 def remove_bid(employeeId, cyclePositionId):
   return requests.delete(f"{API_ROOT}/bids?cp_id={cyclePositionId}&perdet_seq_num={employeeId}")
+
+def fsbid_bid_to_talentmap_bid(data):
+  return {
+    "id": "",
+    "bidcycle": data['cycle']['description'],
+    "emp_id": data['employee']['perdet_seq_num'],
+    "user": "",
+    "bid_statistics": [
+      {
+        "id": "",
+        "bidcycle": data['cycle']['description'],
+        "total_bids": data['cyclePosition']['totalBidders'],
+        "in_grade": data['cyclePosition']['atGradeBidders'],
+        "at_skill": data['cyclePosition']['inConeBidders'],
+        "in_grade_at_skill": data['cyclePosition']['inBothBidders'],
+        "has_handshake_offered": data['cyclePosition']['status'] == 'HS',
+        "has_handshake_accepted": data['cyclePosition']['status'] == 'HS'
+      }
+    ],
+    "position": {
+      "id": data['cyclePosition']['pos_seq_num'],
+      "position_number": data['cyclePosition']['pos_seq_num'],
+      "grade": "",
+      "skill": "",
+      "bureau": "",
+      "title": "",
+      "create_date": data['submittedDate'],
+      "update_date": "",
+      "post": {
+        "id": "",
+        "location": {
+          "id": "",
+          "country": "",
+          "code": "",
+          "city": "",
+          "state": ""
+        }
+      }
+    }
+  }
