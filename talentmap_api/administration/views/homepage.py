782425ae7de9196fa9e9d1521cce3f133e7e0eca
Rob Tirserio <rtirserio@gmail.com>
2019-07-25 16:29:58 -0400

    end of sprint 13 (#111)
    * feature: handle errors that fsbid may raise when submitting a bid
    
    * adds endpoint to view list of logs available and get the contents of a particular log
    
    * remove unnecessary imports
    
    * log entry model managed should be false
    
    * fixing linting issues
    
    * additional lint errors fixed
    
    * adding endpoints for running data sync ad hoc
    
    * forcing superuser permission and removing unused imports and functions
    
    * fix: remove posted date from PV. TM-909 (#94)
    
    * feature: add endpoints for getting and updating the about page content. TM-912 (#93)
    
    * feature: add endpoints for getting and updating the about page content. TM-912
    
    * chore: update comment
    
    * Fix/realign pv values. TM-909 (#96)
    
    * fix: remove posted date from PV. TM-909
    
    * fix: projected vacancy data matching new FSBid endpoints. TM-909
    
    * fix: a few updates from pr feedback
    
    * added endpoint to run update_relationships command (#95)
    
    * adding endpoint for update_string_representations command (#98)
    
    * adding endpoint for update_string_representations command
    
    * lint issue
    
    * feature: allow for saving projected vacancies searches. TM-642
    
    * fix: properly parse langualge string. correct field name error
    
    * fix: remove null entries from lang. list
    
    * fix: syntax
    
    * feature: pv filtering for bureau hierarchy. TM-950
    
    * chore: missed files
    
    * chore: missed this as well
    
    * Update apps.py
    
    * fix: strip the end of the language text so there is no extra spaces
    
    * chore: fix test data
    
    * fix: fix PV favorites by using the id of the pv
    
    * fix: send response object when no results are returned
    
    * adding endpoints to edit SynchronizationJobs in DB and reset SynchronizationJob
    
    * add use_last_date_updated as writable field, remove unnecesary filters
    
    * removing reference to deleted filter class
    
    * fix: correctly filter by location codes. TM-949
    
    * fix: update the shape of the pv object to match new position shape (#109)
    
    * fix: update availability to use cycle position to allow bidding
    
    * updating response from data_sync list for conistency
    
    * fix: use correct filter synatx
    
    * fix: process multiple filter values
    
    * fix: query bids correctly for determining availability

---
+++ talentmap_api/administration/views/homepage.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status, mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from talentmap_api.common.permissions import isDjangoGroupMemberOrReadOnly
+
+from talentmap_api.administration.models import HomepageBanner
+from talentmap_api.administration.serializers.homepage import HomepageBannerSerializer
+
+class HomepageBannerView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet, APIView):
+
+    serializer_class = HomepageBannerSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, isDjangoGroupMemberOrReadOnly('superuser'))
+    
+    def get_queryset(self):
+        return HomepageBanner.objects.first()
+
+    def retrieve(self, request, pk=None, format=None):
+        '''
+        Gets the HomepageBanner
+        '''
+        queryset = get_object_or_404(HomepageBanner)
+        return Response({ "text" : queryset.text, "is_visible": queryset.is_visible })
+
+    def partial_update(self, request, pk=None, format=None):
+        '''
+        Updates the HomepageBanner
+        '''
+        hpb = HomepageBanner.objects.first()
+        serializer = self.serializer_class(hpb, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
