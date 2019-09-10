5401f727092a473e1cd7e87756c717f0765acd0f
Rob Tirserio <rtirserio@gmail.com>
2019-09-10 16:07:51 -0400

    feature: new model for available position designations which includes highlighting. TM-695

--- talentmap_api/available_positions/tests/test_available_position_endpoints.py
+++ talentmap_api/available_positions/tests/test_available_position_endpoints.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 
 from unittest.mock import Mock, patch
 
@@ -34,3 +35,51 @@ def test_favorite_action_endpoints(authorized_client, authorized_user):
       response = authorized_client.get(f'/api/v1/available_position/{ap["cp_id"]}/favorite/')
 
       assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db(transaction=True)
+def test_highlight_action_endpoints(authorized_client, authorized_user):
+
+    response = authorized_client.get(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    # First, try to highlight without appropriate permissions
+    response = authorized_client.put(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    # Now, try to unhiglight without appropriate permissions
+    response = authorized_client.delete(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    # Add the permission to our user
+    group = mommy.make('auth.Group', name='superuser')
+    group.user_set.add(authorized_user)
+
+    response = authorized_client.put(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    response = authorized_client.get(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    response = authorized_client.delete(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    response = authorized_client.get(f'/api/v1/available_position/{ap["cp_id"]}/highlight/')
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db(transaction=True)
+def test_designation_action_endpoints(authorized_client, authorized_user):
+
+    response = authorized_client.patch(f'/api/v1/available_position/{ap["cp_id"]}/designation/', data=json.dumps({"is_hard_to_fill": False}), content_type="application/json")
+
+    assert response.status_code == status.HTTP_200_OK
+
+
