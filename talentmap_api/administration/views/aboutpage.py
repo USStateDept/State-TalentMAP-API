decfd359c1c3899900be8036e5372d26bdd19f8e
Rob Tirserio <rtirserio@gmail.com>
2019-10-01 11:30:21 -0400

    fix: add the about page editor group to the allowed permissions

--- talentmap_api/administration/views/aboutpage.py
+++ talentmap_api/administration/views/aboutpage.py
@@ -15,7 +15,7 @@ from talentmap_api.administration.serializers.aboutpage import AboutPageSerializ
 class AboutPageView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet, APIView):
 
     serializer_class = AboutPageSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, isDjangoGroupMemberOrReadOnly('superuser'))
+    permission_classes = (IsAuthenticatedOrReadOnly, isDjangoGroupMemberOrReadOnly('superuser'), isDjangoGroupMemberOrReadOnly('about_editor'))
 
 
     def get_queryset(self):
