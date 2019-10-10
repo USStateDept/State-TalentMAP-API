9d5874e697dad8dfdd6834dde7c9df44ad7eb63a
Rob Tirserio <rtirserio@gmail.com>
2019-10-10 08:35:43 -0400

    fix: allow sorting on posted_date

--- talentmap_api/fsbid/services/common.py
+++ talentmap_api/fsbid/services/common.py
@@ -109,7 +109,8 @@ sort_dict = {
     "position__grade": "pos_grade_code",
     "position__bureau": "pos_bureau_short_desc",
     "ted": "ted",
-    "position__position_number": "pos_num_text"
+    "position__position_number": "pos_num_text",
+    "posted_date": "cp_post_dt"
 }
 
 
