e5c56f422dec04aadbd80dcb015740017b30f55c
Rob Tirserio <rtirserio@gmail.com>
2019-11-06 08:45:47 -0500

    feature: download and properly stream zipped files to the log download

--- talentmap_api/log_viewer/services.py
+++ talentmap_api/log_viewer/services.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 import os
+from zipfile import ZipFile
 
 from talentmap_api.settings import get_delineated_environment_variable
 log_dir = get_delineated_environment_variable('DJANGO_LOG_DIRECTORY', '/var/log/talentmap/')
@@ -18,12 +19,18 @@ def get_log_list():
 
 def get_log(log_name):
     lines = ""
-    try:
-        with open(log_dir + '/' + log_name + ".log", 'r') as f:
-            lines = f.read()
-    except FileNotFoundError as e:
-        return None
-
+    file_name = f"{log_dir}{log_name}"
+    if os.path.exists(f"{file_name}.log"):
+        try:
+            with open(f"{file_name}.log", 'r') as f:
+                lines = f.read()
+        except FileNotFoundError as e:
+            return None
+    elif os.path.exists(f"{file_name}.zip"):
+        with ZipFile(f"{file_name}.zip") as myzip:
+            for f in myzip.namelist():
+                with myzip.open(f) as myfile:
+                    lines = myfile.read()
     return {
         "data": lines
     }
