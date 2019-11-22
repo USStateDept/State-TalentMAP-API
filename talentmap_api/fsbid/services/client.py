a902f8687ebe3673c68538a299d9e8835edcfea5
scott062 <scott.m.burdick@gmail.com>
2019-11-22 14:18:35 -0500

    Notes

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -1,3 +1,14 @@
 import requests
 import logging
 
+
+# what do we need? 
+# need to verify we have a cdo fetching the other cdos
+# ad_id required for the req
+# jwt required the req
+
+# Are there any common methods that I'm missing? 
+# Services are methods for the api calls? 
+# Views are the actual controllers that map onto the RESTful API
+
+# What do the views/controllers need?
