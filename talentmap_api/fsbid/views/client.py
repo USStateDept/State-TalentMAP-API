02744ebb488f6e4c2fa10e42b03ab98a8202a3c0
scott062 <scott.m.burdick@gmail.com>
2019-11-22 14:05:15 -0500

    Add view client file

---
+++ talentmap_api/fsbid/views/client.py
@@ -0,0 +1,5 @@
+from talentmap_api.fsbid.views.base import BaseView
+import talentmap_api.fsbid.services.reference as services
+import talentmap_api.fsbid.services.common as common
+
+Class FSBidClientView(baseview):
