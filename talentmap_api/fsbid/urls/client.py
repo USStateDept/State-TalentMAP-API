86dc68840fe8f8d69aef3327950c8dc598c0135d
scott062 <scott.m.burdick@gmail.com>
2019-11-22 13:40:50 -0500

    Add urls for fsbid

---
+++ talentmap_api/fsbid/urls/client.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from rest_framework import routers
+
+from talentmap_api.fsbid.views import client as views
+
+router = routers.SimpleRouter()
+
+urlpatterns = [
+    url(r'^perdet_seq_num/$', views.FSBidClientActionView.as_view(), name='client.FSBid-perdet_seq_num-actions'),
+]
+
+urlpatterns += router.urls
