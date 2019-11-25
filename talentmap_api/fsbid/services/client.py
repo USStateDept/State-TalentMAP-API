1a0d726a61bec81e86cf74b70eee81165a46e2e8
scott062 <scott.m.burdick@gmail.com>
2019-11-25 10:54:17 -0500

    Update method to call helper function for converting response to talentmap formatting

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -27,7 +27,7 @@ def agents(jwt_token)
     url = f"{API_ROOT}/Client/Agents?ad_id={ad_id}&rl_cd=CDO"
     response = requests.get(url, headers={'JWTAuthorization': jwt_token, 'Content-Type': 'application/json'}, verify=False).json()  # nosec
     response.raise_for_status()
-    return response
+    return fsbid_cdo_list_to_talentmap_cdo_list(response)
 
 def fsbid_cdo_list_to_talentmap_cdo_list(data)
     return {
