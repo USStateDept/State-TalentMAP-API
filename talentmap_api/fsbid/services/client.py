2a97311f630574e4a81f9d617d331ab9d1c6f339
scott062 <scott.m.burdick@gmail.com>
2019-11-25 13:29:13 -0500

    Remove comments

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -8,17 +8,6 @@ API_ROOT = settings.FSBID_API_URL
 
 logger = logging.getLogger(__name__)
 
-# what do we need? 
-# need to verify we have a cdo fetching the other cdos
-# ad_id required for the req
-# jwt required the req
-
-# Are there any common methods that I'm missing? 
-# Services are methods for the api calls? 
-# Views are the actual controllers that map onto the RESTful API
-
-# What do the views/controllers need?
-
 def agents(jwt_token)
     '''
     Get All CDOs 
