7181963f39d1e0837ff19d268b61102ca25dcc2c
scott062 <scott.m.burdick@gmail.com>
2019-11-25 10:44:06 -0500

    Update views for clients get_agents

--- talentmap_api/fsbid/views/client.py
+++ talentmap_api/fsbid/views/client.py
@@ -1,5 +1,16 @@
 from talentmap_api.fsbid.views.base import BaseView
-import talentmap_api.fsbid.services.reference as services
+import talentmap_api.fsbid.services.client as services
 import talentmap_api.fsbid.services.common as common
 
-Class FSBidClientView(baseview):
+from talentmap_api.common.permissions import isDjangoGroupMember
+# do we have a cdo user permission? 
+
+# base view vs api view???
+Class FSBidAgentView(baseview)
+    permission_classes = (IsAuthenticated, isDjangoGroupMember('cdo'),)
+
+    def get(self, request, pk)
+        '''
+        Get All CDOs (agents)
+        '''
+        return Response(services.agents(request.query_params.get('CDO', None), request.META['HTTP_JWT']))
