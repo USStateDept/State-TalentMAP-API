44a3c19851405864fa168ead51aa460c1ceb29b4
scott062 <scott.m.burdick@gmail.com>
2019-12-02 14:30:38 -0500

    Update naming and endpoints to match web services

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -8,18 +8,3 @@ API_ROOT = settings.FSBID_API_URL
 
 logger = logging.getLogger(__name__)
 
-def cdo(jwt_token):
-    '''
-    Get All CDOs 
-    '''
-    ad_id = jwt.decode(jwt_token, verify=False).get('unique_name')
-    uri = f"/Client/Agents?ad_id={ad_id}&rl_cd=CDO"
-    respone = services.get_fsbid_results(uri, jwt_token, fsbid_cdo_list_to_talentmap_cdo_list)
-    return response
-
-def fsbid_cdo_list_to_talentmap_cdo_list(data):
-    return {
-        "id": data.get("hru_id", None),
-        "name": data.get("fullname", None),
-        "email": data.get("email", None),
-    }
--- talentmap_api/fsbid/views/client.py
+++ talentmap_api/fsbid/views/client.py
@@ -7,12 +7,3 @@ from rest_framework.response import Response
 from talentmap_api.fsbid.views.base import BaseView
 from talentmap_api.common.permissions import isDjangoGroupMember
 from rest_framework.permissions import IsAuthenticated
-# do we have a cdo user permission? 
-
-class FSBidCDOListView(BaseView):
-
-    def get(self, request):
-        '''
-        Gets all cdos
-        '''
-        return Response(services.cdo(request.META['HTTP_JWT']))
