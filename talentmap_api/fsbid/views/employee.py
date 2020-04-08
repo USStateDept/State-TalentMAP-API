984da1e3c661ef95468bd293e5ef00d0a484132e
Mike Joyce <mjoyce91@gmail.com>
2020-04-08 15:19:51 -0400

    Always map roles, even when emp_id is None

--- talentmap_api/fsbid/views/employee.py
+++ talentmap_api/fsbid/views/employee.py
@@ -21,23 +21,21 @@ class FSBidEmployeePerdetSeqNumActionView(BaseView):
         '''
         jwt = request.META['HTTP_JWT']
         emp_id = services.get_employee_perdet_seq_num(jwt)
-        if emp_id is None:
-          return Response(status=status.HTTP_404_NOT_FOUND)
 
-        user = request.user.profile
-        user.emp_id = str(int(emp_id))
-        user.save()
+        if emp_id is not None:
+            user = request.user.profile
+            user.emp_id = str(int(emp_id))
+            user.save()
 
         auth_user = request.user
         # Get the role from the token
-        print('outside loop')
         for current_role in services.map_group_to_fsbid_role(jwt):
           auth_user.groups.add(current_role)
           # Remove any roles that the user may have had but have been removed but retain the TM specific roles
           for tm_role in services.ROLE_MAPPING.values():
             if current_role.name != tm_role:
               auth_user.groups.remove(Group.objects.filter(name=tm_role).first())
-          
+
         auth_user.save()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
