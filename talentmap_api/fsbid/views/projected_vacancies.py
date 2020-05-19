793ee98560efa73e8e2634b61c7470501d49caa3
scott062 <scott.m.burdick@gmail.com>
2020-05-19 12:05:57 -0400

    Finalize ordering and params audit

--- talentmap_api/fsbid/views/projected_vacancies.py
+++ talentmap_api/fsbid/views/projected_vacancies.py
@@ -42,10 +42,10 @@ class FSBidProjectedVacanciesListView(BaseView):
             coreapi.Field("language_codes", location='query', description='Language code'),
             coreapi.Field("position__bureau__code__in", location='query', description='Bureau Code'),
             coreapi.Field("position__grade__code__in", location='query', description='Grade Code'),
+            coreapi.Field("position__position_number__in", location='query', description='Position Numbers'),
             coreapi.Field("position__post__code__in", location='query', description='Post id'),
             coreapi.Field("position__post__danger_pay__in", location='query', description='Danger pay'),
             coreapi.Field("position__post__differential_rate__in", location='query', description='Diff. Rate'),
-            coreapi.Field("position__position_number__in", location='query', description='Position Numbers'),
             coreapi.Field("position__post_indicator__in", location='query', description='Use name values from /references/postindicators/'),
             coreapi.Field("position__post__tour_of_duty__code__in", location='query', description='TOD code'),
             coreapi.Field("position__skill__code__in", location='query', description='Skill Code'),
@@ -70,7 +70,9 @@ class FSBidProjectedVacanciesTandemListView(BaseView):
             coreapi.Field("ordering", location='query', description='Ordering),
             coreapi.Field("page", location='query', description='Page Index'),
             coreapi.Field("limit", location='query', description='Page Limit'),
-            
+
+            coreapi.Field("getCount", location='query', description='Results Count'),
+
             # Tandem 1
             coreapi.Field("id", location="query", description="Projected Vacancies ids"),
             coreapi.Field("is_available_in_bidseason", location='query', description='Bid Season id'),
