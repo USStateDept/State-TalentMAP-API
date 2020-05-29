0f460d0a18571ce44e724633a3da3273fbc8be09
Mike Joyce <mjoyce91@gmail.com>
2020-05-29 13:48:19 -0400

    TALENTMAP -> talentmap

--- create-oracle-user.sql
+++ create-oracle-user.sql
@@ -1,4 +1,4 @@
 ALTER SESSION SET CONTAINER = orclpdb1;
 GRANT connect, resource to talentmap identified by talentmap container=current;
 GRANT CREATE SESSION TO talentmap container=current;
-GRANT ALL PRIVILEGES TO TALENTMAP;
+GRANT ALL PRIVILEGES TO talentmap;
