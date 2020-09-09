39e434d9381d9ab71404bd9e65a9af321437fbeb
Mike Joyce <mjoyce91@gmail.com>
2020-09-09 15:06:03 -0400

    Add endpoint that will track position views, and allow superusers to see the statistics

--- talentmap_api/stats/filters.py
+++ talentmap_api/stats/filters.py
@@ -1,7 +1,7 @@
 import rest_framework_filters as filters
 
-from talentmap_api.stats.models import LoginInstance
-from talentmap_api.common.filters import DATETIME_LOOKUPS
+from talentmap_api.stats.models import LoginInstance, ViewPositionInstance
+from talentmap_api.common.filters import DATETIME_LOOKUPS, ALL_TEXT_LOOKUPS
 
 
 class LoginInstanceFilter(filters.FilterSet):
@@ -10,4 +10,14 @@ class LoginInstanceFilter(filters.FilterSet):
         model = LoginInstance
         fields = {
             "date_of_login": DATETIME_LOOKUPS,
+        }
+
+class ViewPositionInstanceFilter(filters.FilterSet):
+
+    class Meta:
+        model = ViewPositionInstance
+        fields = {
+            "date_of_view": DATETIME_LOOKUPS,
+            "position_id": ALL_TEXT_LOOKUPS,
+            "position_type": ALL_TEXT_LOOKUPS,
         }
