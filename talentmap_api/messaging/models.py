c19b4e6210c2f5d413c20862826743521c158df6
Mike Joyce <mjoyce91@gmail.com>
2020-10-05 14:23:54 -0400

    Add metadata to notification model

--- talentmap_api/messaging/models.py
+++ talentmap_api/messaging/models.py
@@ -14,6 +14,7 @@ class Notification(models.Model):
     message = models.TextField(help_text="The message for the notification")
     owner = models.ForeignKey('user_profile.UserProfile', on_delete=models.CASCADE, null=False, related_name="notifications", help_text="The owner of the notification")
     tags = JSONField(default=[], help_text="Tags to categorize the notification")
+    meta = models.TextField(default='{}', help_text="Meta data about the notification") # Has to be TextField due to ORA-01754
 
     is_read = models.BooleanField(default=False, help_text="Whether this notification has been read")
 
