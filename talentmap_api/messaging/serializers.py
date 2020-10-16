23805d32cb93aee85a911c567b0009dedc7b87ff
Mike Joyce <mjoyce91@gmail.com>
2020-10-16 09:52:21 -0400

    Catch when meta field is empty and return object

--- talentmap_api/messaging/serializers.py
+++ talentmap_api/messaging/serializers.py
@@ -19,5 +19,8 @@ class NotificationSerializer(PrefetchedSerializer):
 
     def to_representation(self, data):
         data = super(NotificationSerializer, self).to_representation(data)
-        data['meta'] = json.loads(data.get('meta'))
-        return data
+        try:
+            data['meta'] = json.loads(data.get('meta'))
+            return data
+        except:
+            return {}
