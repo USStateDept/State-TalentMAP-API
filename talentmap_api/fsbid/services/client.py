5ea1201cedddfd81ddaec7a2fdf8e9d6bfb2254c
Cory Covington <ccovington@metaphaseconsulting.com>
2021-02-25 19:04:18 -0500

    added function for middle name & updating logic

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -234,15 +234,12 @@ def fsbid_clients_to_talentmap_clients(data):
     except:
         initials = None
 
-    middle_name = employee.get('per_middle_name', None) or ''
-    middle_initial = ''
-    if middle_name:
-        middle_initial = middle_name[:1] + ' '
+    middle_name = get_middle_name(employee)
 
     return {
         "id": employee.get("pert_external_id", None),
-        "name": f"{employee.get('per_first_name', None)} {middle_name} {employee.get('per_last_name', None)}",
-        "shortened_name": f"{employee.get('per_first_name', None)} {middle_initial}{employee.get('per_last_name', None)}",
+        "name": f"{employee.get('per_first_name', None)} {middle_name[0]}{employee.get('per_last_name', None)}",
+        "shortened_name": f"{employee.get('per_first_name', None)} {middle_name[1]}{employee.get('per_last_name', None)}",
         "initials": initials,
         "perdet_seq_number": employee.get("perdet_seq_num", None),
         "grade": employee.get("per_grade_code", None),
@@ -265,14 +262,16 @@ def fsbid_clients_to_talentmap_clients_for_csv(data):
     employee = data.get('employee', None)
     current_assignment = employee.get('currentAssignment', None)
     pos_location = None
+    middle_name = get_middle_name(employee)
     if current_assignment is not None:
         position = current_assignment.get('currentPosition', None)
         if position is not None:
             pos_location = map_location(position.get("currentLocation", None))
 
+
     return {
         "id": employee.get("perdet_seq_num", None),
-        "name": f"{employee.get('per_first_name', None)} {employee.get('per_last_name', None)}",
+        "name": f"{employee.get('per_first_name', None)} {middle_name[0]}{employee.get('per_last_name', None)}",
         "grade": employee.get("per_grade_code", None),
         "skills": ' , '.join(map_skill_codes_for_csv(employee)),
         "employee_id": employee.get("pert_external_id", None),
@@ -284,6 +283,15 @@ def fsbid_clients_to_talentmap_clients_for_csv(data):
         # "noBids": fsbid_no_bids_to_tmap(data.get("no_bids")),
         "classifications": fsbid_classifications_to_tmap(employee.get("classifications", []))
     }
+
+
+def get_middle_name(employee):
+    middle_name = employee.get('per_middle_name', None) or ''
+    middle_initial = ''
+    if middle_name:
+        middle_name = middle_name + ' '
+        middle_initial = middle_name[:1] + ' '
+    return [middle_name, middle_initial]
 
 
 def hru_id_filter(query):
