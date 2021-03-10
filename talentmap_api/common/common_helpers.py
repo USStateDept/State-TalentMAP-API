fcff1f3b2ae1d8e699d162167d01655692cbb4a6
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-03-10 15:05:38 -0500

    Update common_helpers.py

--- talentmap_api/common/common_helpers.py
+++ talentmap_api/common/common_helpers.py
@@ -448,10 +448,6 @@ def formatCSV(data, fieldsInfo):
 
     for f in fieldsInfo:
         if f is "skills":
-            print("============")
-            print(f)
-            print(list(data))
-            print("============")
             skills = []
             for skill in list(data[f]):
                 skills.append(skill["description"])
