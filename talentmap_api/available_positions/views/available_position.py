fc933a8c551c658a7f4265e7bee8f42cdb7ac0ac
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-05-11 17:32:15 -0400

    cleanup

--- talentmap_api/available_positions/views/available_position.py
+++ talentmap_api/available_positions/views/available_position.py
@@ -414,11 +414,8 @@ class BureauBiddersRankings(APIView):
         """
         Return position information for all of bidders' bids including their ranking information for those positions
         """
-        # 1. grab user bidlist
         user_bids = bidservices.user_bids(pk, request.META['HTTP_JWT'])
-        # 2. grab users' rankings, excluding current position
         user_rankings = AvailablePositionRanking.objects.filter(bidder_perdet=pk).exclude(cp_id=cp_id).order_by('rank')
-        # 3. filter out bids not on shortlist
         shortlist_bids = list(filter(lambda x: (user_rankings.filter(
             cp_id=str(pydash.get(x, 'position.id'))).exists()), user_bids))
         filtered_bids = []
@@ -426,13 +423,10 @@ class BureauBiddersRankings(APIView):
             pos_id = str(pydash.get(bid, 'position.id'))
             hasBureauPermissions = empservices.has_bureau_permissions(pos_id, self.request.META['HTTP_JWT'])
             hasOrgPermissions = empservices.has_org_permissions(pos_id, self.request.META['HTTP_JWT'])
-            # 4. filter out based on bureau/org perms
             if hasOrgPermissions or hasBureauPermissions:
-                # audit how much data is being sent to FE and how much we actually need
                 bid["ranking"] = user_rankings.filter(cp_id=pos_id).values_list("rank", flat=True).first()
                 filtered_bids.append(bid)
 
-        # keep in mind that the ranking comes back as -1 the UI-Ranking
         return Response({
             "results": filtered_bids,
             "other-sl-bidcount": len(shortlist_bids) - len(filtered_bids),
