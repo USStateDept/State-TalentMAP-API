12dd0b7bcd1f8ca49153f7f496345d2898dff06b
Mike Joyce <joyce_michael@bah.com>
2021-05-25 14:59:32 -0400

    only return the active perdet

--- talentmap_api/bidding/services/bidhandshake.py
+++ talentmap_api/bidding/services/bidhandshake.py
@@ -8,19 +8,15 @@ logger = logging.getLogger(__name__)
 
 def get_position_handshake_data(cp_id):
     '''
-    Return whether the cycle position is active and to which perdet, if any, possesses the active handshake
+    Return whether the cycle position is active, and if so, to which perdet possesses the active handshake
     '''
     props = {
-        'active': False,
         'active_handshake_perdet': None,
     }
 
     hs = BidHandshake.objects.filter(cp_id=cp_id).values()
-    active = pydash.filter_(hs, lambda x: x['status'] is not 'R')
-    if len(active) is 0:
-        props['active'] = True
     
-    active = pydash.find(hs, lambda x: x['status'] is 'O' or x['status'] is 'A')
+    active = pydash.find(hs, lambda x: x['status'] is not 'R')
     if active:
         props['active_handshake_perdet'] = active['bidder_perdet']
 
