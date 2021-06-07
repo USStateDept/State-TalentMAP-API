164e3bbc51fff083ba4951cae45c404325a7b476
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-06-07 16:14:20 -0400

    Update common.py

--- talentmap_api/fsbid/services/common.py
+++ talentmap_api/fsbid/services/common.py
@@ -198,9 +198,7 @@ def get_individual(uri, id, query_mapping_function, jwt_token, mapping_function,
     Gets an individual record by the provided ID
     '''
     response = get_results(uri, {"id": id}, query_mapping_function, jwt_token, mapping_function, api_root)
-    if response is None:
-        return response
-    return response[0]
+    return pydash.get(response, '[0]') or None
 
 
 def send_get_request(uri, query, query_mapping_function, jwt_token, mapping_function, count_function, base_url, host=None, api_root=API_ROOT):
