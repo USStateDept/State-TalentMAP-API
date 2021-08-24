159e311468ea4ae682d14b0227cd2cb77115a0c3
Mike Joyce <joyce_michael@bah.com>
2021-08-24 09:05:59 -0400

    Add export endpoint for glossary (#591)
    Co-authored-by: SidelineCory24 <55964163+SidelineCory24@users.noreply.github.com>

--- talentmap_api/glossary/views/glossary.py
+++ talentmap_api/glossary/views/glossary.py
@@ -1,3 +1,7 @@
+import csv
+
+from django.http import HttpResponse
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from rest_framework.viewsets import GenericViewSet
@@ -6,6 +10,7 @@ from rest_framework import mixins
 from talentmap_api.common.common_helpers import get_prefetched_filtered_queryset
 from talentmap_api.common.mixins import FieldLimitableSerializerMixin
 from talentmap_api.common.common_helpers import in_group_or_403
+from talentmap_api.fsbid.views.base import BaseView
 
 from talentmap_api.glossary.models import GlossaryEntry
 from talentmap_api.glossary.filters import GlossaryEntryFilter
@@ -51,3 +56,25 @@ class GlossaryView(FieldLimitableSerializerMixin,
 
     def get_queryset(self):
         return get_prefetched_filtered_queryset(GlossaryEntry, self.serializer_class)
+
+
+class GlossaryCSVView(BaseView):
+    """
+    get:
+    Download a CSV of the entire glossary
+    """
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, serializer):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=TalentMAP-Glossary.csv'
+
+        writer = csv.writer(response)
+        writer.writerow(['Title', 'Definition', 'URL'])
+
+        glossary = GlossaryEntry.objects.exclude(is_archived=True).values_list('title', 'definition', 'link')
+        for glossary_entry in glossary:
+            writer.writerow(glossary_entry)
+
+        return response
