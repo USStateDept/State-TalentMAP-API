9096602d18d04f1e1762d8bfba5dc47e5305558d
scott062 <scott.m.burdick@gmail.com>
2021-10-26 14:54:25 -0400

    Change ap - pos and default empty score

--- talentmap_api/fsbid/services/positions.py
+++ talentmap_api/fsbid/services/positions.py
@@ -22,21 +22,22 @@ def get_position(id, jwt_token):
         fsbid_pos_to_talentmap_pos
     )
 
-def fsbid_pos_to_talentmap_pos(ap):
+def fsbid_pos_to_talentmap_pos(pos):
     '''
     Converts the response generic position from FSBid to a format more in line with the Talentmap position
     '''
-    r1 = ap.get("pos_read_proficiency_1_code", None) or '--'
-    s1 = ap.get("pos_speak_proficiency_1_code", None) or '--'
-    l1 = ap.get("pos_language_1_desc", None)
+    empty_score = '--'
+    r1 = pos.get("pos_read_proficiency_1_code", None) or empty_score
+    s1 = pos.get("pos_speak_proficiency_1_code", None) or empty_score
+    l1 = pos.get("pos_language_1_desc", None)
     rep1 = f"{l1} {r1}/{s1}"
-    r2 = ap.get("pos_read_proficiency_2_code", None) or '--'
-    s2 = ap.get("pos_speak_proficiency_2_code", None) or '--'
-    l2 = ap.get("pos_language_2_desc", None)
+    r2 = pos.get("pos_read_proficiency_2_code", None) or empty_score
+    s2 = pos.get("pos_speak_proficiency_2_code", None) or empty_score
+    l2 = pos.get("pos_language_2_desc", None)
     rep2 = f"{l2} {r2}/{s2}"
 
     return {
-        "id": ap.get("pos_seq_num", None),
+        "id": pos.get("pos_seq_num", None),
         "status": None,
         "status_code": None,
         "ted": None,
@@ -47,16 +48,16 @@ def fsbid_pos_to_talentmap_pos(ap):
         },
         "tandem_nbr": None,  # Only appears in tandem searches
         "position": {
-            "id": ap.get("pos_seq_num", None),
-            "grade": ap.get("pos_grade_code", None),
-            "skill": f"{ap.get('pos_skill_desc', '')} {ap.get('pos_skill_code')}",
-            "skill_code": ap.get("pos_skill_code", None),
+            "id": pos.get("pos_seq_num", None),
+            "grade": pos.get("pos_grade_code", None),
+            "skill": f"{pos.get('pos_skill_desc', '')} {pos.get('pos_skill_code')}",
+            "skill_code": pos.get("pos_skill_code", None),
             "skill_secondary": None,
             "skill_secondary_code": None,
-            "bureau": f"({ap.get('pos_bureau_short_desc', None)}) {ap.get('pos_bureau_long_desc', None)}",
-            "bureau_code": ap.get('pos_bureau_code', None),
-            "organization": f"({ap.get('pos_org_short_desc', None)}) {ap.get('pos_org_long_desc', None)}",
-            "organization_code": ap.get('pos_org_code', None),
+            "bureau": f"({pos.get('pos_bureau_short_desc', None)}) {pos.get('pos_bureau_long_desc', None)}",
+            "bureau_code": pos.get('pos_bureau_code', None),
+            "organization": f"({pos.get('pos_org_short_desc', None)}) {pos.get('pos_org_long_desc', None)}",
+            "organization_code": pos.get('pos_org_code', None),
             "tour_of_duty": None,
             "classifications": None,
             "representation": None,
@@ -64,13 +65,13 @@ def fsbid_pos_to_talentmap_pos(ap):
                 "availability": None,
                 "reason": None
             },
-            "position_number": ap.get("pos_num_text", None),
-            "title": ap.get("pos_title_desc", None),
+            "position_number": pos.get("pos_num_text", None),
+            "title": pos.get("pos_title_desc", None),
             "is_overseas": None,
             "is_highlighted": None,
-            "create_date": ap.get("pos_create_date", None),
-            "update_date": ap.get("pos_update_date", None),
-            "effective_date": ap.get("pos_effective_date", None),
+            "create_date": pos.get("pos_create_date", None),
+            "update_date": pos.get("pos_update_date", None),
+            "effective_date": pos.get("pos_effective_date", None),
             "posted_date": None,
             "description": {
                 "id": None,
@@ -95,22 +96,22 @@ def fsbid_pos_to_talentmap_pos(ap):
             },
             "post": {
                 "id": None,
-                "code": ap.get("pos_location_code", None),
+                "code": pos.get("pos_location_code", None),
                 "tour_of_duty": None,
                 "post_overview_url": None,
                 "post_bidding_considerations_url": None,
                 "cost_of_living_adjustment": None,
-                "differential_rate": ap.get("bt_differential_rate_num", None),
-                "danger_pay": ap.get("bt_danger_pay_num", None),
+                "differential_rate": pos.get("bt_differential_rate_num", None),
+                "danger_pay": pos.get("bt_danger_pay_num", None),
                 "rest_relaxation_point": None,
                 "has_consumable_allowance": None,
                 "has_service_needs_differential": None,
-                "obc_id": services.get_obc_id(ap.get("pos_location_code", None)),
+                "obc_id": services.get_obc_id(pos.get("pos_location_code", None)),
                 "location": {
-                    "country": ap.get("location_country", None),
-                    "code": ap.get("pos_location_code", None),
-                    "city": ap.get("location_city", None),
-                    "state": ap.get("location_state", None),
+                    "country": pos.get("location_country", None),
+                    "code": pos.get("pos_location_code", None),
+                    "city": pos.get("location_city", None),
+                    "state": pos.get("location_state", None),
                 },
             },
             "latest_bidcycle": {
