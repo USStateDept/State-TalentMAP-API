a3cdd2f383485920798f86bddb943f6552fa4bee
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-11-08 13:17:06 -0500

    HS Register info (#603)
    * temp while ws adds
    
    * updated for mock
    
    * Update bureau.py
    
    Co-authored-by: Mike Joyce <joyce_michael@bah.com>

--- talentmap_api/fsbid/services/bureau.py
+++ talentmap_api/fsbid/services/bureau.py
@@ -201,6 +201,9 @@ def fsbid_bureau_position_bids_to_talentmap(bid, jwt, cp_id, active_perdet):
         "ted": ted,
         "has_handshake_offered": hasHandShakeOffered,
         "submitted_date": ensure_date(bid.get('ubw_submit_dt'), utc_offset=-5),
+        # fsbid hs offered stats are for register, not to be confused with TM HS functionality
+        "handshake_registered": bid.get('handshake_offered_flag'),
+        "handshake_registered_date": ensure_date(bid.get('handshake_offered_dt'), utc_offset=-5),
         "cdo": cdo,
         "classifications": classifications,
         "has_competing_rank": has_competing_rank_value,
