971c1f7d5854eb082d3380d8979435f2fbd242e2
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-03-08 16:31:31 -0500

    Update agenda.py

--- talentmap_api/fsbid/urls/agenda.py
+++ talentmap_api/fsbid/urls/agenda.py
@@ -8,7 +8,6 @@ router = routers.SimpleRouter()
 urlpatterns = [
     url(r'^agenda_items/$', views.AgendaItemView.as_view(), name="agenda-agenda-items"),
     url(r'^agenda_items/export/$', views.AgendaItemCSVView.as_view(), name="agenda-export-agenda-items"),
-    url(r'^statuses/$', views.AgendaItemCSVView.as_view(), name="agenda-export-agenda-items")
 ]
 
 urlpatterns += router.urls
