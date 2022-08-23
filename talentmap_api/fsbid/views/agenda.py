9104d4f002cb3b9c699a6b34c04635849f6ffbdb
Scott Burdick <scott.m.burdick@gmail.com>
2022-08-23 19:48:00 -0400

    Remove unnecessary pk argument

--- talentmap_api/fsbid/views/agenda.py
+++ talentmap_api/fsbid/views/agenda.py
@@ -73,7 +73,7 @@ class AgendaItemActionView(BaseView):
                  }), description='Legs'),
     }))
 
-    def post(self, request, pk):
+    def post(self, request):
         '''
         Create single agenda
         '''
