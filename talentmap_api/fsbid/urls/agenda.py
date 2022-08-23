0247e33b9cdbb1b2a7f31115c2bfdec53d389506
Scott Burdick <scott.m.burdick@gmail.com>
2022-08-23 19:18:23 -0400

    Update singular action url

--- talentmap_api/fsbid/urls/agenda.py
+++ talentmap_api/fsbid/urls/agenda.py
@@ -8,6 +8,7 @@ router = routers.SimpleRouter()
 urlpatterns = [
     url(r'^agenda_items/(?P<pk>[0-9]+)/$', views.AgendaItemView.as_view(), name="agenda-agenda-item"),
     url(r'^agenda_items/$', views.AgendaItemListView.as_view(), name="agenda-agenda-items"),
+    url(r'^agenda_item/$', views.AgendaItemActionView.as_view(), name="agenda-agenda-item"),
     url(r'^agenda_items/export/$', views.AgendaItemCSVView.as_view(), name="agenda-export-agenda-items"),
     url(r'^remarks/$', views.AgendaRemarksView.as_view(), name="agenda-reference-remarks"),
     url(r'^remark-categories/$', views.AgendaRemarkCategoriesView.as_view(), name="agenda-reference-remark-categories"),
