ccc829370e1fcbf3a272cad97da3021f93641b5f
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-01-23 12:00:31 -0500

    TM Panel Statuses and Types EP setup

--- talentmap_api/fsbid/services/panel.py
+++ talentmap_api/fsbid/services/panel.py
@@ -11,12 +11,16 @@ PANEL_API_ROOT = settings.PANEL_API_URL
 
 logger = logging.getLogger(__name__)
 
-def fsbid_to_talentmap_panel(data, default_data, add_these):
+def fsbid_to_talentmap_panel(data, default_data, add_these=[]):
     # default_data: default data points (opinionated EP)
     # add_these: additional data points
 
     cols_mapping = {
         'pm_seq_num': 'pmdpmseqnum',
+        # these can be multiple values - so we'd want pmdseqnum and pmseqnum to map
+        # to pm_seq_num
+        # pmt_code should go to pmpmtcode and pmtcode
+        # check this assumption
         'pmd_dttm': 'pmddttm',
         'pm_virtual': 'pmvirtualind',
         'pm_create_id': 'pmcreateid',
@@ -31,7 +35,7 @@ def fsbid_to_talentmap_panel(data, default_data, add_these):
         'pmi_create_date': 'pmicreatedate',
         'pmi_update_id': 'pmiupdateid',
         'pmi_update_date': 'pmiupdatedate',
-        'pmt_code': 'pmpmtcode',
+        'pmt_code': 'pmtcode',
         'pmt_desc_text': 'pmtdesctext',
         'pmt_create_id': 'pmtcreateid',
         'pmt_create_date': 'pmtcreatedate',
@@ -93,6 +97,7 @@ def get_panel_dates(query, jwt_token):
 
     return panel
 
+
 def convert_panel_dates_query(query):
     '''
     Converts TalentMap query into FSBid query
@@ -102,6 +107,86 @@ def convert_panel_dates_query(query):
         "rp.pageNum": int(query.get("page", 1)),
         "rp.pageRows": int(query.get("limit", 1000)),
         "rp.filter": services.convert_to_fsbid_ql([{'col': 'pmdmdtcode', 'val': 'MEET'}]),
+    }
+
+    valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
+
+    return urlencode(valuesToReturn, doseq=True, quote_via=quote)
+
+
+def get_panel_statuses(query, jwt_token):
+    '''
+    Get panel statuses
+    '''
+
+    hard_coded = ['pms_code', 'pms_desc_text']
+
+    args = {
+        "uri": "references/statuses",
+        "query": query,
+        "query_mapping_function": convert_panel_statuses_query,
+        "jwt_token": jwt_token,
+        "mapping_function": partial(fsbid_to_talentmap_panel, default_data=hard_coded),
+        "count_function": None,
+        "base_url": "/api/v1/panels/",
+        "api_root": PANEL_API_ROOT,
+    }
+
+    statuses = services.send_get_request(
+        **args
+    )
+
+    return statuses
+
+
+def convert_panel_statuses_query(query):
+    '''
+    Converts TalentMap query into FSBid query
+    '''
+
+    values = {
+        "rp.pageNum": int(query.get("page", 1)),
+        "rp.pageRows": int(query.get("limit", 1000)),
+    }
+
+    valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
+
+    return urlencode(valuesToReturn, doseq=True, quote_via=quote)
+
+
+def get_panel_types(query, jwt_token):
+    '''
+    Get panel types
+    '''
+
+    hard_coded = ['pmt_desc_text', 'pmt_code']
+
+    args = {
+        "uri": "references/types",
+        "query": query,
+        "query_mapping_function": convert_panel_types_query,
+        "jwt_token": jwt_token,
+        "mapping_function": partial(fsbid_to_talentmap_panel, default_data=hard_coded),
+        "count_function": None,
+        "base_url": "/api/v1/panels/",
+        "api_root": PANEL_API_ROOT,
+    }
+
+    types = services.send_get_request(
+        **args
+    )
+
+    return types
+
+
+def convert_panel_types_query(query):
+    '''
+    Converts TalentMap query into FSBid query
+    '''
+
+    values = {
+        "rp.pageNum": int(query.get("page", 1)),
+        "rp.pageRows": int(query.get("limit", 1000)),
     }
 
     valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
@@ -132,6 +217,7 @@ def get_panel_categories(query, jwt_token):
     )
     return panel_cats
 
+
 def convert_panel_category_query(query):
     '''
     Converts TalentMap query into FSBid query
@@ -147,20 +233,24 @@ def convert_panel_category_query(query):
     return urlencode(valuesToReturn, doseq=True, quote_via=quote)
 
 
-
 def get_panel_meetings(query, jwt_token):
     '''
     Get panel meetings
     '''
+
+    hard_coded = ['pm_seq_num', 'pm_virtual', 'pm_create_id', 'pm_create_date',
+                    'pm_update_id', 'pm_update_date', 'pms_code', 'pmt_code',
+                    'pmt_desc_text', 'pms_desc_text', 'panelMeetingDates' ]
+
     args = {
-        "uri": "",
-        "query": query,
-        "query_mapping_function": convert_panel_category_query,
-        "jwt_token": jwt_token,
-        "mapping_function": fsbid_to_talentmap_panel_categories,
-        "count_function": None,
-        "base_url": "/api/v1/panels/",
-        "api_root": PANEL_API_ROOT,
+    "uri": "",
+    "query": query,
+    "query_mapping_function": convert_panel_category_query,
+    "jwt_token": jwt_token,
+    "mapping_function": partial(fsbid_to_talentmap_panel, default_data=hard_coded),
+    "count_function": None,
+    "base_url": "/api/v1/panels/",
+    "api_root": PANEL_API_ROOT,
     }
 
     panel_cats = services.send_get_request(
@@ -174,29 +264,26 @@ def convert_panel_category_query(query):
     Converts TalentMap query into FSBid query
     '''
 
+    #  from the ui it looks like the date will actually be a date range.
+
     values = {
         "rp.pageNum": int(query.get("page", 1)),
         "rp.pageRows": int(query.get("limit", 1000)),
+        "rp.filter": services.convert_to_fsbid_ql([
+            {'col': 'pmpmtcode', 'val': query.get("type", None)},
+            {'col': 'pmddttm', 'val': query.get("date", None)},
+            {'col': 'pmscode', 'val': query.get("status", None)}
+        ]),
     }
+
+
+    #             openapi.Parameter("page", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='A page number within the paginated result set.'),
+    #             openapi.Parameter("limit", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='Number of results to return per page.'),
+    #             openapi.Parameter("type", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='Panel meeting type.'),
+    #             openapi.Parameter("date", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='Date of the Panel meeting.'),
+    #             openapi.Parameter("status", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='Panel meeting status.'),
+    #         ])
 
     valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
 
     return urlencode(valuesToReturn, doseq=True, quote_via=quote)
-
-
-def fsbid_to_talentmap_panel_categories(data):
-    # hard_coded are the default data points (opinionated EP)
-    # add_these are the additional data points we want returned
-
-    hard_coded = ['mic_code', 'mic_desc_text', 'pmt_code']
-
-    add_these = []
-
-    cols_mapping = {
-        'mic_code': 'miccode',
-        'mic_desc_text': 'micdesctext',
-    }
-
-    add_these.extend(hard_coded)
-
-    return services.map_return_template_cols(add_these, cols_mapping, data)
--- talentmap_api/fsbid/urls/panel.py
+++ talentmap_api/fsbid/urls/panel.py
@@ -7,10 +7,12 @@ from talentmap_api.fsbid.views import agenda as agenda_views
 router = routers.SimpleRouter()
 
 urlpatterns = [
-    url(r'^dates/$', views.PanelDatesView.as_view(), name='panel-FSBid-dates'),
     url(r'^categories/$', views.PanelCategoriesView.as_view(), name='panel-FSBid-categories'),
+    url(r'^dates/$', views.PanelDatesView.as_view(), name='panel-FSBid-dates'),
+    url(r'^statuses/$', views.PanelStatusesView.as_view(), name='panel-FSBid-statuses'),
+    url(r'^types/$', views.PanelTypesView.as_view(), name='panel-FSBid-types'),
+    url(r'^meetings/', views.PanelMeetingsView.as_view(), name="panel-meetings-list"),
     url(r'^(?P<pk>[0-9]+)/agendas/', agenda_views.PanelAgendasListView.as_view(), name="panel-agendas-list"),
-    url(r'^meetings/', views.PanelMeetingsView.as_view(), name="panel-agenda-list"),
 ]
 
 urlpatterns += router.urls
