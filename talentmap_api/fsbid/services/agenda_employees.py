4a8765601cc0b7a3dcdd6e9de43924c9d2076764
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-02-08 14:10:16 -0500

    adding back 'or none' and refactoring variables with none default

--- talentmap_api/fsbid/services/agenda_employees.py
+++ talentmap_api/fsbid/services/agenda_employees.py
@@ -145,23 +145,23 @@ def convert_agenda_employees_query(query):
     tedStart = query.get("ted-start")
     tedEnd = query.get("ted-end")
 
-    firstName = query.get("firstName").upper() if query.get("firstName") else query.get("firstName")
-    lastName = query.get("lastName").upper() if query.get("lastName") else query.get("lastName")
-    empID = query.get("empID").upper() if query.get("empID") else query.get("empID")
+    firstName = query.get("firstName").upper() if query.get("firstName") else None
+    lastName = query.get("lastName").upper() if query.get("lastName") else None
+    empID = query.get("empID").upper() if query.get("empID") else None
 
     activeCodes = "S,L,A,P,U" if not query.get("isInactiveSelected") else None
     
     filters = [
-        {"col": "tmpercurrentbureaucode", "com": "IN", "val": query.get("current-bureaus", None)},
-        {"col": "tmperhsbureaucode", "com": "IN", "val": query.get("handshake-bureaus", None)},
-        {"col": "tmpercurrentorgcode", "com": "IN", "val": query.get("current-organizations", None)},
-        {"col": "tmperhsorgcode", "com": "IN", "val": query.get("handshake-organizations", None)},
-        {"col": "tmpercdoid", "com": "IN", "val": query.get("cdos", None)},
-        {"col": "tmperperscode", "com": "IN", "val": "S,L,A,P,U"},
-        {"col": "tmperperdetseqnum", "com": "EQ", "val": query.get("perdet", None)},
-        {"col": "tmperperfirstname", "com": "CONTAINS", "val": query.get("firstName", None)},
-        {"col": "tmperperlastname", "com": "CONTAINS", "val": query.get("lastName", None)},
-        {"col": "tmperpertexternalid", "com": "EQ", "val": query.get("empID", None)}
+        {"col": "tmpercurrentbureaucode", "com": "IN", "val": query.get("current-bureaus") or None},
+        {"col": "tmperhsbureaucode", "com": "IN", "val": query.get("handshake-bureaus") or None},
+        {"col": "tmpercurrentorgcode", "com": "IN", "val": query.get("current-organizations") or None},
+        {"col": "tmperhsorgcode", "com": "IN", "val": query.get("handshake-organizations") or None},
+        {"col": "tmpercdoid", "com": "IN", "val": query.get("cdos") or None},
+        {"col": "tmperperscode", "com": "IN", "val": activeCodes},
+        {"col": "tmperperdetseqnum", "com": "EQ", "val": query.get("perdet") or None},
+        {"col": "tmperperfirstname", "com": "CONTAINS", "val": firstName},
+        {"col": "tmperperlastname", "com": "CONTAINS", "val": lastName},
+        {"col": "tmperpertexternalid", "com": "EQ", "val": empID}
     ]
 
     if query.get("handshake"):
