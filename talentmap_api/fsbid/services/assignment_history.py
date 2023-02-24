b448841654dd61338e3c4c2df216c8dbf5e5b892
Cory Covington <ccovington@metaphaseconsulting.com>
2023-02-24 10:33:05 -0500

    typo fix

--- talentmap_api/fsbid/services/assignment_history.py
+++ talentmap_api/fsbid/services/assignment_history.py
@@ -44,7 +44,7 @@ def assignment_history_to_client_format(data):
     if type(assignmentsCopy) is type([]):
         for x in assignmentsCopy:
             pos = pydash.get(x, 'position[0]') or {}
-            loc = pydash.get(pos, 'location[0]') or []
+            loc = pydash.get(pos, 'location[0]') or {}
             tmap_assignments.append(
                 {
                     "id": x['id'],
