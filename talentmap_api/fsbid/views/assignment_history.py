6bb0a78b738c72666d2de4b379e5907310fab9d0
Scott Burdick <scott.m.burdick@gmail.com>
2023-04-14 15:40:04 -0400

    Add ep for asg history by logged in user

--- talentmap_api/fsbid/views/assignment_history.py
+++ talentmap_api/fsbid/views/assignment_history.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 from rest_condition import Or
 
 from talentmap_api.fsbid.views.base import BaseView
+from talentmap_api.user_profile.models import UserProfile
 from talentmap_api.fsbid.services.assignment_history import get_assignments, assignment_history_to_client_format
 from talentmap_api.common.permissions import isDjangoGroupMember
 
@@ -30,6 +31,25 @@ class FSBidAssignmentHistoryListView(BaseView):
         '''
         query_copy = request.query_params.copy()
         query_copy["perdet_seq_num"] = pk 
+        query_copy._mutable = False
+        data = assignment_history_to_client_format(get_assignments(query_copy, request.META['HTTP_JWT']))
+        return Response(data)
+
+
+class FSBidPrivateAssignmentHistoryListView(BaseView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter("page", openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='A page number within the paginated result set.'),
+            openapi.Parameter('limit', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='Number of results to return per page.')
+        ])
+
+    def get(self, request):
+        '''
+        Gets current users assignment history
+        '''
+        user = UserProfile.objects.get(user=self.request.user)
+        query_copy = request.query_params.copy()
+        query_copy["perdet_seq_num"] = user.emp_id
         query_copy._mutable = False
         data = assignment_history_to_client_format(get_assignments(query_copy, request.META['HTTP_JWT']))
         return Response(data)
