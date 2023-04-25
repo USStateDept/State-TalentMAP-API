bd5673792d81a86b45b3d461a5a3732530c8f9b2
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2023-04-25 13:45:43 -0400

    code review

--- talentmap_api/fsbid/views/persons.py
+++ talentmap_api/fsbid/views/persons.py
@@ -2,9 +2,7 @@ from talentmap_api.common.permissions import isDjangoGroupMember
 import talentmap_api.fsbid.services.persons as services
 from talentmap_api.fsbid.views.base import BaseView
 from rest_framework.response import Response
-from drf_yasg.utils import swagger_auto_schema
 from rest_condition import Or
-from drf_yasg import openapi
 
 class FSBidPersonsView(BaseView):
     permission_classes = [Or(isDjangoGroupMember('ao_user'), isDjangoGroupMember('cdo')), ]
