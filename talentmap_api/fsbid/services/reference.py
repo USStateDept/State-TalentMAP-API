4642c6ff7630c24e88c98401168f2ef61e485717
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2023-05-09 17:00:05 -0400

    revert changes to old TOD endpoint

--- talentmap_api/fsbid/services/reference.py
+++ talentmap_api/fsbid/services/reference.py
@@ -102,8 +102,12 @@ def fsbid_languages_to_talentmap_languages(data):
 @staticmethod
 def fsbid_tour_of_duties_to_talentmap_tour_of_duties(data):
     return {
+        "id": data.get("code", 0),
         "code": data.get("code", 0),
+        "is_active": True,
+        "months": None,
         "long_description": data.get("long_desc", None),
+        "short_description": data.get("long_desc", None)
     }
 
 
