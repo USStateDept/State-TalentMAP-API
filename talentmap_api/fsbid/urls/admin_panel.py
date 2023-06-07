a95a1f62b69b11a52c56bfa91655ad6a29be638e
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-06-07 12:34:01 -0400

    changing name to match pattern

--- talentmap_api/fsbid/urls/admin_panel.py
+++ talentmap_api/fsbid/urls/admin_panel.py
@@ -6,7 +6,7 @@ from talentmap_api.fsbid.views import admin_panel as views
 router = routers.SimpleRouter()
 
 urlpatterns = [
-    url(r'^remark/$', views.CreateRemarkView.as_view(), name='administration.createremark'),
+    url(r'^remark/$', views.CreateRemarkView.as_view(), name='administration-createremark'),
 ]
 
 urlpatterns += router.urls
