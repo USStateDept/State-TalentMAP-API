79ffdc752e95b75a39eaf0917bc4cf1d40e0f26c
Scott Burdick <scott.m.burdick@gmail.com>
2023-06-26 15:48:56 -0400

    Fix error

--- talentmap_api/projected_vacancies/tests/mommy_recipes.py
+++ talentmap_api/projected_vacancies/tests/mommy_recipes.py
@@ -1,4 +1,5 @@
 from model_mommy import mommy
+from model_mommy.recipe import Recipe
 
 from talentmap_api.user_profile.models import UserProfile
 from talentmap_api.projected_vacancies.models import ProjectedVacancyFavorite
