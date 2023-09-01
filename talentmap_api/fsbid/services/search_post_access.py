072890d848a69afb17adb7b3acff870c945e9188
Scott Burdick <scott.m.burdick@gmail.com>
2023-09-01 12:19:32 -0400

    Update mapping again

--- talentmap_api/fsbid/services/search_post_access.py
+++ talentmap_api/fsbid/services/search_post_access.py
@@ -33,50 +33,45 @@ def spa_filter_req_mapping(request):
         "PV_AD_ID_I": '',
     }
 
-def get_filter_data_from_response(data, data_map):
-    filter_data = []
-    for item in data:
-        new_entry = {}
-        for key, value in item.items():
-            if key in data_map and (len(new_entry) != 2):
-                new_entry[data_map[key]] = value
-        filter_data.append(new_entry)
-    return filter_data
-
-
 def spa_filter_res_mapping(data):
-    bureau_map = {
-        'Bureau': 'code',
-        'ORG_SHORT_DESC': 'description',
-    }
-    person_map = {
-        'PER_SEQ_NUM': 'code',
-        'PER_FULL_NAME': 'description',
-    }
-    role_map = {
-        'ROLE_CODE': 'code',
-        'ROLE_DESC': 'description',
-    }
-    org_map = {
-        'Org': 'code',
-        'ORG_DESC': 'description',
-    }
-    location_map = {
-        'COUNTRY_STATE_CODE': 'code',
-        'COUNTRY_STATE_DESC': 'description',
-    }
-    position_map = {
-        'POS_SEQ_NUM': 'code',
-        'POS_NUM_TEXT': 'description',
-    }
+    def bureau_map(x):
+        return {
+            'code': x.get('Bureau'),
+            'description': x.get('ORG_SHORT_DESC'),
+        }
+    def person_map(x):
+        return {
+            'code': x.get('PER_SEQ_NUM'),
+            'description': x.get('PER_FULL_NAME'),
+        }
+    def role_map(x):
+        return {
+            'code': x.get('ROLE_CODE'),
+            'description': x.get('ROLE_DESC'),
+        }
+    def org_map(x):
+        return {
+            'code': x.get('Org'),
+            'description': x.get('ORG_DESC'),
+        }
+    def location_map(x):
+        return {
+            'code': x.get('COUNTRY_STATE_CODE'),
+            'description': x.get('COUNTRY_STATE_DESC'),
+        }
+    def position_map(x):
+        return {
+            'code': x.get('POS_SEQ_NUM'),
+            'description': x.get('POS_NUM_TEXT'),
+        }
 
     return {
-        'bureauFilters': get_filter_data_from_response(data['PQRY_BUREAU_LEVEL_O'], bureau_map),
-        'personFilters': get_filter_data_from_response(data['PQRY_PERSON_LEVEL_O'], person_map),
-        'roleFilters': get_filter_data_from_response(data['PQRY_POST_ROLE_O'], role_map),
-        'positionFilters': get_filter_data_from_response(data['PQRY_POSITION_LEVEL_O'], position_map),
-        'locationFilters': get_filter_data_from_response(data['PQRY_COUNTRY_O'], location_map),
-        'orgFilters': get_filter_data_from_response(data['PQRY_ORG_LEVEL_O'], org_map),
+        'bureauFilters': list(map(bureau_map, data.get('PQRY_BUREAU_LEVEL_O'))),
+        'personFilters': list(map(person_map, data.get('PQRY_PERSON_LEVEL_O'))),
+        'roleFilters': list(map(role_map, data.get('PQRY_POST_ROLE_O'))),
+        'positionFilters': list(map(position_map, data.get('PQRY_POSITION_LEVEL_O'))),
+        'locationFilters': list(map(location_map, data.get('PQRY_COUNTRY_O'))),
+        'orgFilters': list(map(org_map, data.get('PQRY_ORG_LEVEL_O'))),
     }
 
 
@@ -98,20 +93,18 @@ def get_search_post_access_data(jwt_token, request):
 
 
 def fsbid_to_tm_spa_data_mapping(data):
-    data = data['PQRY_ORG_ACCESS_O']
-
-    def mapping_helper(x):
+    def spa_results_mapping(x):
         return {
-            'bureau': x.get('BUREAUNAME'),
-            'post': x.get('ORGNAME'),
-            'employee': x.get('PERFULLNAME'),
-            'id': x.get('BOAID'),
-            'access_type': x.get('BAT_DESCR_TXT'),
-            'role': x.get('ROLEDESCR'),
-            'title': x.get('POS_TITLE_DESC'),
-            'position': x.get('POS_NUM_TEXT'),
+            'bureau': x.get('BUREAUNAME') or '---',
+            'post': f"{x.get('ORGNAME')} ({x.get('ORGCODE')})",
+            'employee': x.get('PERFULLNAME') or '---',
+            'id': x.get('BOAID') or '---',
+            'access_type': x.get('BAT_DESCR_TXT') or '---',
+            'role': x.get('ROLEDESCR') or '---',
+            'title': x.get('POS_TITLE_DESC') or '---',
+            'position': x.get('POS_NUM_TEXT') or '---',
         }
-    return list(map(mapping_helper, data))
+    return list(map(spa_results_mapping, data.get('PQRY_ORG_ACCESS_O')))
 
 
 def format_request_data_to_string(request_values, table_key):
@@ -132,13 +125,15 @@ def map_search_post_access_query(req):
         mapped_request['PJSON_EMP_TAB_I'] = format_request_data_to_string(req.get('persons'), 'PER_SEQ_NUM')
     if req.get('bureaus'):
         mapped_request['PJSON_BUREAU_TAB_I'] = format_request_data_to_string(req.get('bureaus'), 'BUREAU_ORG_CODE')
-    if req.get('posts'):
-        mapped_request['PJSON_COUNTRY_TAB_I'] = format_request_data_to_string(req.get('posts'), 'COUNTRY_STATE_CODE')
+    if req.get('locations'):
+        mapped_request['PJSON_COUNTRY_TAB_I'] = format_request_data_to_string(req.get('locations'), 'COUNTRY_STATE_CODE')
     if req.get('roles'):
         mapped_request['PJSON_POST_ROLE_TAB_I'] = format_request_data_to_string(req.get('roles'), 'ROLE_CODE')
     if req.get('orgs'):
-        # TODO - orgs has a bug in FSBID and needs ORG_SHORT_DESC as well
-        mapped_request['PJSON_ORG_TAB_I'] = format_request_data_to_string(req.get('orgs'), 'ORG_CODE')
+        mapped_request['PJSON_ORG_TAB_I'] = format_request_data_to_string(req.get('orgs'), 'ORG_SHORT_DESC')
+    if req.get('positions'):
+        mapped_request['PJSON_POS_DD_TAB_I'] = format_request_data_to_string(req.get('positions'), 'POS_SEQ_NUM')
+
     return mapped_request
 
 
